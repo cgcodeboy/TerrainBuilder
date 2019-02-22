35c8c2ee0d308d0240e1b0972158436448541b7e
cgcodeboy <cgcodeboy@163.com>
2019-02-22 16:46:39 +0800

    add voronoi generate function

--- Version_1/include/voronoigenerator.h
+++ Version_1/include/voronoigenerator.h
@@ -2,15 +2,30 @@
 #define VORONOIGENERATOR_H
 
 #include <include/generator.h>
+#include <include/voronoiwidget.h>
+
+#include <vector>
 
 class VoronoiGenerator : public Generator
 {
+    Q_OBJECT
 public:
     VoronoiGenerator();
 
     QRectF boundingRect() const;
 
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+private:
+    virtual void heightDataProcess();
+
+    CImg<unsigned char> m_heightData;
+    VoronoiWidget *_voronoi;
+
+    int _number;
+
+private slots:
+    void infoCome(int number);
 
 protected:
     virtual void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event);
--- Version_1/src/voronoigenerator.cpp
+++ Version_1/src/voronoigenerator.cpp
@@ -5,6 +5,9 @@ VoronoiGenerator::VoronoiGenerator():Generator(ITEM_TYPE::Voronoi_Generator)
     focused = false;
     setFlag(ItemIsSelectable,true);
     setFlag(ItemIsFocusable,true);
+
+    _voronoi = new VoronoiWidget;
+    connect(_voronoi,SIGNAL(infoConfirm(int)),this,SLOT(infoCome(int)));
 }
 
 QRectF VoronoiGenerator::boundingRect() const
@@ -37,8 +40,56 @@ void VoronoiGenerator::paint(QPainter *painter, const QStyleOptionGraphicsItem *
     }
 }
 
+void VoronoiGenerator::heightDataProcess()
+{
+    //create points
+    std::vector<QPoint> points;
+    srand(time(NULL));
+    int w = 255 - 20, h = 255 - 20;
+    for (int i = 0; i < _number; i++)
+    {
+        QPoint point;
+        point.setX(rand()%w+10);
+        point.setY(rand()%w+10);
+        points.push_back(point);
+    }
+
+    m_heightData.assign(255,255,1,1);
+    int d;
+    h = w = 255;
+    for (int hh = 0; hh < h; hh++) {
+        for (int ww = 0; ww < w; ww++) {
+            int ind = -1, dist = INT_MAX;
+            for (int it = 0; it < points.size(); it++) {
+                QPoint p = points[it];
+                d = sqrt((p.x()-hh)*(p.x()-hh)+(p.y()-ww)*(p.y()-ww));
+                if (d < dist) {
+                    dist = d;
+                    ind = it;
+                }
+            }
+
+            if (ind > -1){
+                int value = 0 + dist * log(_number);
+                if(value  < 255)
+                    m_heightData(hh, ww, 0, 0) = 255 - value;
+                else
+                    m_heightData(hh, ww, 0, 0) = 0;
+            }
+        }
+    }
+    m_heightData.save_bmp(("./tmp/image/"+getName()+".bmp").toStdString().c_str());
+}
+
+void VoronoiGenerator::infoCome(int number)
+{
+    _number = number;
+    heightDataProcess();
+}
+
 void VoronoiGenerator::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
 {
+    _voronoi->show();
     QGraphicsItem::mouseDoubleClickEvent(event);
 }
 
